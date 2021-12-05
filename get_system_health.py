5cfcbced1ebbf24f0d43d95b96c20174547c913e
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2021-12-05 02:02:34 +0100

    lets go

--- get_system_health.py
+++ get_system_health.py
@@ -1,6 +1,22 @@
 # -- coding: utf-8 --
 
 import psutil
+import json
 
-memory = psutil.virtual_memory()
-print(memory)
+raw_memory = psutil.virtual_memory()
+
+memory_total = round(raw_memory.total * 0.000000001, 2)
+memory_available = round(raw_memory.available * 0.000000001, 2)
+memory_free = round(raw_memory.free * 0.000000001, 2)
+
+message = {
+  "cpu": {
+    "total": memory_total,
+    "available": memory_available,
+    "free": memory_free,
+  }
+}
+
+system_health = json.dumps(message)
+
+print(system_health)
