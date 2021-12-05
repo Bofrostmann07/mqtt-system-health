64b2e78867591bf85d92848f173ec7b24544f74f
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2021-12-05 03:18:44 +0100

    lets go

--- get_system_health.py
+++ get_system_health.py
@@ -2,21 +2,61 @@
 
 import psutil
 import json
+#import context  # Ensures paho-mqtt is in PYTHONPATH
+import paho.mqtt.publish as publish
+import platform
+
+
+hostname = platform.node()
+system_name = platform.system()
+machine_name = platform.machine()
+processor_type = platform.processor()
 
 raw_memory = psutil.virtual_memory()
+raw_cpu = psutil.cpu_percent(interval=1)
+raw_disk = psutil.disk_usage('/')
+
+cpu_utilization = raw_cpu
 
 memory_total = round(raw_memory.total * 0.000000001, 2)
-memory_available = round(raw_memory.available * 0.000000001, 2)
 memory_free = round(raw_memory.free * 0.000000001, 2)
+memory_used = round(raw_memory.used * 0.000000001, 2)
+memory_used_percent = raw_memory.percent
+
+disk_total = round(raw_disk.total * 0.000000001, 2)
+disk_free = round(raw_disk.free * 0.000000001, 2)
+disk_used = round(raw_disk.used * 0.000000001, 2)
+disk_used_percent = raw_disk.percent
 
 message = {
+  "general": {
+  "hostname": hostname,
+  "system": system_name,
+  "machine": machine_name,
+  "processor": processor_type
+  },
   "cpu": {
+    "utilization": cpu_utilization
+  },
+  "memory": {
     "total": memory_total,
-    "available": memory_available,
     "free": memory_free,
+    "used": memory_used,
+    "used_percent": memory_used_percent
+  },
+  "disk": {
+    "total": disk_total,
+    "free": disk_free,
+    "used": disk_used,
+    "used_percent": disk_used_percent
   }
 }
 
 system_health = json.dumps(message)
+mqtt_topic = "system_health/" + hostname
+publish.single(mqtt_topic, system_health, hostname="10.30.0.3")
 
-print(system_health)
+
+
+
+#print(test)
